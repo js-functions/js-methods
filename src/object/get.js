798b3aa7734d48af194af2d8ae94348e60927157
Pravin Pareekh <pareek.pravin@gmail.com>
2021-03-13 12:31:42 +0530

    Get method
    Implementation of get meth to access object value by string path.

--- src/object/get.js
+++ src/object/get.js
@@ -1,11 +1,19 @@
 /**
- * 
+ * 1. Replace [] with .
+ * 2. Split .
+ * 3. Remove double .'s or blank string
+ * 4. Map the path
  * @param {*} object 
  * @param {*} path 
  * @returns {*} return resolved value
  */
 function get(object, path) {
-  return 4
+  if (typeof object !== 'object') throw new TypeError('First parameter should be object type')
+  if (typeof path !== 'string') throw new TypeError('Second parameter should be string type')
+  return path.replace(/\[|\]\.?/g, '.')  // https://tinyurl.com/yjgybc9g
+    .split('.')
+    .filter(p => p)
+    .reduce((t, v) => t && t[v], object)
 }
 
 module.exports = get
