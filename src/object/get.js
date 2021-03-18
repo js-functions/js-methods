c3aa3888023797dbd25f0216716b0ad6ed18caac
Pravin Pareekh <pareek.pravin@gmail.com>
2021-03-18 12:16:50 +0530

    Get method
    Implementation to return default value if object path is undefined

--- src/object/get.js
+++ src/object/get.js
@@ -1,20 +1,22 @@
 /**
- * Gets the value at `path` of `object`.
+ * Gets the value at `path` of `object`, and if path not found return the default value
  * 1. Replace [] with .
  * 2. Split .
  * 3. Remove double .'s or blank string
  * 4. Map the path
- * @param {*} object 
- * @param {*} path 
- * @returns {*} return resolved value
+ * @param {Object} object Source object
+ * @param {String} string Path to pick
+ * @param {*} [defaultValue] The value returned for 'undefined' path
+ * @returns {*} Returns resolved value
  */
-function get(object, path) {
+function get(object, path, defaultValue) {
   if (typeof object !== 'object') throw new TypeError('First parameter should be object type')
   if (typeof path !== 'string') throw new TypeError('Second parameter should be string type')
-  return path.replace(/\[|\]\.?/g, '.')  // https://tinyurl.com/yjgybc9g
+  const result = path.replace(/\[|\]\.?/g, '.')  // https://tinyurl.com/yjgybc9g
     .split('.')
     .filter(p => p)
     .reduce((t, v) => t && t[v], object)
+  return result === undefined ? defaultValue : result
 }
 
 module.exports = get
