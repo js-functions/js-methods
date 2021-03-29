4dcb53755e4104c0e287e9b2638c6c33669fcb05
Rajesh Jadhav <rajeshjadhav.rj@gmail.com>
2021-03-29 15:31:42 +0530

    removed the unwanted commented code

--- src/array/flattenDeep.js
+++ src/array/flattenDeep.js
@@ -3,43 +3,12 @@
  * @param {Array} array
  * @returns {Array} Returns new flattened array
  */
-let flattenedArray = []
-// function arrayIteration(array, flattenedArray){
-//   array.map((item) => {
-//     const length = item.length
-//     // check if item is array using array length property
-//     // if undefined, means it is not an array then push into flattenedArray
-//     if (length === undefined) {
-//       flattenedArray.push(item)
-//     } else {
-//       // if array length is not undefined (actual length) means it is a array
-//       // then recursively call itself 
-//       arrayIteration(item, flattenedArray)
-//     }
-//   }) 
-// }
-
-function flattenDeep(array) {
-  // i don't want declare flattendArray variable outside of the outer function 
+function flattenDeep(array) {  
   let flattenedArray = []
   if (!Array.isArray(array)){ 
     throw new TypeError('parameter should be of Array type') 
   }
   
-  function recursiveFunction1(array){
-    // loop through array
-    array.map((item) => {
-      // check if item is array using array length property
-      // if undefined, means it is not an array then push into flattenedArray
-      if (item.length === undefined) {
-        flattenedArray.push(item)
-      } else {
-        // if array length is not undefined (actual length) means it is a array
-        // then recursively call itself 
-        recursiveFunction(item)
-      }
-    })    
-  } 
   function recursiveFunction(array){
     // loop through array
     let i = 0
